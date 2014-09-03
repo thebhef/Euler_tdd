2b68a541d8c1cdaab1aaca5b796aba01bcd0568a
GB Hefner <thebhef@gmail.com>
2014-09-02 23:39:14 -0500

    started on 3

---
+++ C#/3/Euler3_Tests/Euler3_Tests/Euler3_Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Euler3;
+using NUnit.Framework;
+using ProjectEulerInterfaces;
+
+namespace Euler3_Tests
+{
+    /// <summary>
+    /// Euler 3 tests.
+    /// 
+    /// The Problem:
+    /// 
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// 
+    /// </summary>
+
+    [TestFixture]
+    public class Euler3_TestFixture
+    {
+        [Test]
+        public void ReturnsPrimeFactorsAsExpected()
+        {
+            IEulerSolver solver = new Euler3Solver();
+
+            string solution = solver.GetSolution((long)13195).ToString();
+
+            Assert.AreEqual("5,7,13,29", solution);
+        }
+    }
+}
---
+++ C#/CsharpCommon/PrimeGenerator/PrimeNumberGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProjectEulerInterfaces;
+
+namespace PrimeNumberGenerator
+{
+
+    /// <summary>
+    /// Sieve of Eratosthenes + 2 3 5 wheel
+    /// 
+    /// 
+    /// TODO:
+    /// Does ISequenceGenerator work for this? Maybe it's not generic enough? 
+    /// What other kinds of things would be more like this prime number gen?
+    /// How will I test the wheel, or should it be separate?
+    /// 
+    /// </summary>
+
+    public class PrimeGenerator : ISequenceGenerator
+    {
+        #region ISequenceGenerator Members
+        public object[] GetSequence(long Count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] GetSequence(object[] Init, long Count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] UpperBoundedGetSequence(object MaxValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] UpperBoundedGetSequence(object[] Init, object MaxValue)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+    }
+}
